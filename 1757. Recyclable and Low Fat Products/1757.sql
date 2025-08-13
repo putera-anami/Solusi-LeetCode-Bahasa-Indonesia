8d3188f7ea6ea9f07bf1b4e0970c488c875d9bf9
putera-anami <mputeraanami+github@gmail.com>
2025-08-13 13:42:41 +0700

    penambahan komen di soal 584 dan 1757

--- 1757. Recyclable and Low Fat Products/1757.sql
+++ 1757. Recyclable and Low Fat Products/1757.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+SELECT product_id -- pilih kolom product_id.
+FROM Products -- dari tabel Products.
+WHERE low_fats = 'Y' AND recyclable = 'Y'; -- di mana low_fats = 'Y' dan recyclable = 'Y'.
--- 584. Find Customer Referee/584.sql
+++ 584. Find Customer Referee/584.sql
@@ -1,2 +1,4 @@
 # Write your MySQL query statement below
-SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS null;
+SELECT name -- pilih kolom name.
+FROM Customer -- dari tabel Customer.
+WHERE referee_id != 2 OR referee_id IS null; -- di mana referee_id bukan 2 atau referee_id adalah null.
