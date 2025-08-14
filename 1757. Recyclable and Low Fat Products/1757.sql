c8262c73a4cf31d6e1dae2edb136e07ad2bb1aad
putera-anami <mputeraanami+github@gmail.com>
2025-08-14 21:12:04 +0700

    hapus instruksi dari LeetCode di soal 584 dan 1757

--- 1757. Recyclable and Low Fat Products/1757.sql
+++ 1757. Recyclable and Low Fat Products/1757.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 SELECT product_id -- pilih kolom product_id,
 FROM Products -- dari tabel Products,
 WHERE low_fats = 'Y' AND recyclable = 'Y'; -- di mana low_fats = 'Y' dan recyclable = 'Y'.
--- 584. Find Customer Referee/584.sql
+++ 584. Find Customer Referee/584.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
 SELECT name -- pilih kolom name,
 FROM Customer -- dari tabel Customer,
 WHERE referee_id != 2 OR referee_id IS null; -- di mana referee_id bukan 2 atau referee_id adalah null.
