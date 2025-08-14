09f035fefaf9de6b27667433a039febbaaab4b1b
putera-anami <mputeraanami+github@gmail.com>
2025-08-14 21:08:47 +0700

    upload soal 595

---
+++ 595. Big Countries/595.sql
@@ -0,0 +1,3 @@
+SELECT name, population, area -- pilih kolom name, population, dan area,
+FROM World -- dari tabel World,
+WHERE population >= 25000000 OR area >= 3000000; -- di mana population lebih dari sama dengan 25.000.000 atau area lebih dari sama dengan 3.000.000.
