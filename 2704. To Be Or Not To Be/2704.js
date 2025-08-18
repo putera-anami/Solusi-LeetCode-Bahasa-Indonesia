9221a8c0dc4a0d424800b3b57a100bbbda1a3f35
putera-anami <mputeraanami+github@gmail.com>
2025-08-18 16:34:41 +0700

    Upload soal 2704

---
+++ 2704. To Be Or Not To Be/2704.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(i) {
+            if (val === i) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(i) {
+            if (val !== i) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        },
+
+    }
+};
+
+// Di bawah ini kode boilerplate dari LeetCode.
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
+
+// Di bawah ini gak perlu copy ke LeetCode.
+
+// Case yang menghasilkan nilai true.
+// console.log(expect(5).toBe(5));
+// console.log(expect(5).notToBe(4));
+
+// Case yang menghasilkan error.
+// console.log(expect(5).toBe(1));
+// console.log(expect(5).notToBe(5));
