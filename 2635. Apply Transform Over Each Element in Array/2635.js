ac023e9100b44bb29ee2c1f1614a89a128053b95
putera-anami <mputeraanami+github@gmail.com>
2025-08-21 23:13:54 +0700

    upload soal 2635

---
+++ 2635. Apply Transform Over Each Element in Array/2635.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;
+};
+
+// Di bawah ini gak perlu copy ke LeetCode.
+// console.log(map([1, 2, 3], (n, i) => n + 1));
+// console.log(map([1, 2, 3], (n, i) => n + i));
+// console.log(map([10, 20, 30], (n, i) => 42));
+// console.log(map([2, 4, 6], (n, i) => n * n));
