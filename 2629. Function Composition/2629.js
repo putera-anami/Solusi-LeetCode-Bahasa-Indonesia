235b244bbc4505404c2951612ed4a39b1f78fe99
putera-anami <mputeraanami+github@gmail.com>
2025-08-26 16:21:23 +0700

    upload soal 2629

---
+++ 2629. Function Composition/2629.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        for (let fn of functions.reverse()) {
+            x = fn(x);
+        }
+        return x;
+    }
+};
+
+// Di bawah ini kode boilerplate dari LeetCode.
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+// Di bawah ini gak perlu copy ke LeetCode.
+const f = x => x + 1;
+const g = x => x * x;
+const h = x => 2 * x;
+
+const composed = compose([f, g, h]);
+console.log(composed(4));
