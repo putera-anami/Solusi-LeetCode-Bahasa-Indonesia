e79bc750270e5e2eedf4e1d64c536c50452eefb5
putera-anami <mputeraanami+github@gmail.com>
2025-08-27 22:34:17 +0700

    update komentar 1378

--- 1378. Replace Employee ID With The Unique Identifier/1378.sql
+++ 1378. Replace Employee ID With The Unique Identifier/1378.sql
@@ -1,6 +1,6 @@
-SELECT unique_id, name -- -- Pilih kolom unique_id dan name,
+SELECT unique_id, name -- Pilih kolom unique_id dan name,
 FROM Employees -- dari tabel Employees,
 LEFT JOIN EmployeeUNI -- lalu gabungkan dengan tabel EmployeeUNI dengan mengambil semua data dari tabel sebelah kiri (Employees).
-ON Employees.id = EmployeeUNI.id; -- Lalu kondisi di mana kolom id pada Employees sama dengan kolom id pada EmployeeUNI.
+ON Employees.id = EmployeeUNI.id; -- Pada kondisi di mana kolom id di tabel Employees sama dengan kolom id di tabel EmployeeUNI.
 
 -- Jika pada tabel EmployeeUNI tidak ditemukan baris yang cocok, maka data dari tabel Employees tetap muncul, tapi kolom dari EmployeeUNI akan bernilai null.
