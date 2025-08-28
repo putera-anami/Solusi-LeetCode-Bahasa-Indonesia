f913cc18ffa7eb5912e5b0ba882179314975b3ab
putera-anami <mputeraanami+github@gmail.com>
2025-08-28 21:35:21 +0700

    upload soal 2703

---
+++ 2703. Return Length of Arguments Passed/2703.js
@@ -0,0 +1,27 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+
+// Cara 1
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+// Cara 2
+// var argumentsLength = function(...args) {
+//     let count = 0;
+//     for (let i=0; i < args.length; i++) {
+//         count++;
+//     }
+//     return count;
+// };
+
+// Di bawah ini kode boilerplate dari LeetCode.
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+
+// Di bawah ini gak perlu copy ke LeetCode.
+// console.log(argumentsLength(5));
+// console.log(argumentsLength({}, null, "3"));
