76da60c042cd8e5a7d222d07c0fd906951f2b99e
putera-anami <mputeraanami+github@gmail.com>
2025-08-29 18:47:11 +0700

    upload soal 387

---
+++ 387. First Unique Character in a String/387.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const charCounts = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        charCounts[char] = charCounts[char] + 1 || 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (charCounts[char] === 1) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+// Di bawah ini gak perlu di-copy ke LeetCode.
+// console.log(firstUniqChar("leetcode"));
+// console.log(firstUniqChar("loveleetcode"));
+// console.log(firstUniqChar("aabb"));
