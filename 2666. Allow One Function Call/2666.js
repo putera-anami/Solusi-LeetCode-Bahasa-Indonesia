2a97ea7d00e9c6e66f56b82297ca503c3156a9eb
putera-anami <mputeraanami+github@gmail.com>
2025-08-31 23:00:01 +0700

    upload soal 2666

---
+++ 2666. Allow One Function Call/2666.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let called = false;
+    return function(...args){
+        if (called) {
+            return undefined;
+        }
+        called = true;
+        return fn (...args);
+    }
+};
+
+// Di bawah ini kode boilerplate dari LeetCode.
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
+// Di bawah ini gak perlu copy ke LeetCode.
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// const onceSum = once(sum);
+
+// console.log(onceSum(1, 2, 3));
+// console.log(onceSum(4, 5, 6));
+// console.log(onceSum(7, 8, 9));
