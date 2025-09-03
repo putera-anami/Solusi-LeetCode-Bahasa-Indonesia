f40b28bbea26e82a878bcf98c9b2593d76961361
putera-anami <mputeraanami+github@gmail.com>
2025-09-03 23:36:54 +0700

    upload soal 1832

---
+++ 1832. Check if the Sentence Is Pangram/1832.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} sentence
+ * @return {boolean}
+ */
+var checkIfPangram = function(sentence) {
+    const alphabet = new Set();
+
+    for (const char of sentence) {
+        alphabet.add(char);
+    }
+    return alphabet.size === 26;
+};
+
+// Di bawah ini gak perlu di-copy ke LeetCode.
+// const firstSentence = checkIfPangram("abcdefghijklmnopqrstuvwxyz");
+// const secondSentence = checkIfPangram("abcdefghijklmnopqrstuvwxy");
+// const thirdSentence = checkIfPangram("thequickbrownfoxjumpsoverthelazydog");
+// console.log(firstSentence);
+// console.log(secondSentence);
+// console.log(thirdSentence);
