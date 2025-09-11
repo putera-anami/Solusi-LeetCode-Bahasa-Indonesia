6fd60f2737223b92c254b35833cfe03e64caa8c7
putera-anami <mputeraanami+github@gmail.com>
2025-09-11 11:43:42 +0700

    upload soal 2798

---
+++ 2798. Number of Employees Who Met the Target/2798.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let count = 0;
+
+    for (let i = 0; i < hours.length; i++) {
+        if (hours[i] >= target) {
+            count++;
+        }
+    }
+    return count;
+};
+
+// Kode di bawah tidak perlu disalin ke LeetCode.
+// console.log(numberOfEmployeesWhoMetTarget([0,1,2,3,4], 2));
+// console.log(numberOfEmployeesWhoMetTarget([5,1,4,2,2], 6));
