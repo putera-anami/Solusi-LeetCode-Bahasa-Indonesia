f080d7fd695d15459da170bb0ce521a9b3ab5453
putera-anami <mputeraanami+github@gmail.com>
2025-09-26 21:47:07 +0700

    upload soal 2769

---
+++ 2769. Find the Maximum Achievable Number/2769.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    return num + 2 * t;
+};
+
+// Kode di bawah tidak perlu disalin ke LeetCode.
+// console.log(theMaximumAchievableX(4, 1));
+// console.log(theMaximumAchievableX(3, 2));
