c01337a0ee5cc42a32d0a351a562ac2456bd1cf6
putera-anami <mputeraanami+github@gmail.com>
2025-10-28 14:29:11 +0700

    upload soal 2677

---
+++ 2677. Chunk Array/2677.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+        chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+};
+
+// Kode di bawah tidak perlu disalin ke LeetCode.
+console.log(chunk([1, 2, 3, 4, 5], 1));
+console.log(chunk([1, 9, 6, 3, 2], 3));
+console.log(chunk([8, 5, 3, 2, 6], 6));
+console.log(chunk([], 1));
